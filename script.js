eeba77b207e24fdf1b7faf5d5a2ddda7348b4cb5
TimaLebed <timalebedevskiy@mail.ru>
2020-03-16 11:11:39 +0300

    feat: add solution for singolo DOM

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-const HEADER_NAV = document.getElementById('header-nav');
+const headerNav = document.querySelector('#header-nav');
+const links = document.querySelectorAll('.header-navbar__link');
+const portfolioImages = document.querySelectorAll('.portfolio-image__element');
+const portfolioImagesContainer = document.querySelector('.portfolio-image');
+const form = document.querySelector('#form');
+const subjectInput = document.querySelector('#subject-input');
+const textareaInput = document.querySelector('#textarea-input');
+const modalSubjectText = document.querySelector('#modal-subject');
+const modalDescriptionText = document.querySelector('#modal-description');
+const modalButton = document.querySelector('#modal-button');
+const modal = document.querySelector('.modal');
 
-HEADER_NAV.addEventListener('click', (event) => {
-    HEADER_NAV.querySelectorAll('a').forEach(el => el.classList.remove('header-active'));
-    event.target.classList.add('header-active');
+headerNav.addEventListener('click', event => {
+    if (event.target.classList.contains('header-navbar__link')) {
+        links.forEach(el => el.classList.remove('header-active'));
+        event.target.classList.add('header-active');
+    }
+});
+
+portfolioImagesContainer.addEventListener('click', event => {
+    if (event.target.classList.contains('portfolio-image__element')) {
+        portfolioImages.forEach(el => el.classList.remove('portfolio-image-active'));
+        event.target.classList.add('portfolio-image-active');
+    }
+});
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const subjectInputValue = subjectInput.value ? `Тема: ${subjectInput.value}` : 'Без темы';
+    const textareaValue = textareaInput.value ? `Описание: ${textareaInput.value}` : 'Без описания';
+
+    modalSubjectText.innerText = subjectInputValue;
+    modalDescriptionText.innerText = textareaValue;
+
+    modal.classList.remove('hidden');
+});
+
+modalButton.addEventListener('click', event => {
+    modal.classList.add('hidden');
 });
